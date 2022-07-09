5d8fbc3bf271d99a20c8b595cc11b86484109f99
pouyapouryaie <pouyapouryaie@gmail.com>
2022-07-09 08:49:54 +0430

    Jenkins-test Created

---
+++ src/test/java/ir/bigz/jenkinstest/JenkinsTestApplicationTests.java
@@ -0,0 +1,22 @@
+package ir.bigz.jenkinstest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class JenkinsTestApplicationTests {
+
+    public static Logger logger = LoggerFactory.getLogger(JenkinsTestApplicationTests.class);
+
+    @Test
+    void contextLoads() {
+        logger.info("Test case executing ...");
+        assertTrue(true);
+    }
+
+}
